92aece68e2182207fa55b15555bcf1f3ebdf24f8
ehabyanni <ehabayanni@gmail.com>
2022-08-28 15:03:34 +0200

    git commit

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -33,38 +33,40 @@ export class LoginPageComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    if (this.tokenStorage.getToken() != null) {
-      this.isLoggedIn = true;
-    }
-    if (this.isLoggedIn) {
-      this.router.navigate(['home/dashboard']);
+    // if (this.tokenStorage.getToken() != null) {
+    //   this.isLoggedIn = true;
+    // }
+    // if (this.isLoggedIn) {
+    //   this.router.navigate(['home/dashboard']);
 
-    }
+    // }
+    this.router.navigate(['home/dashboard']);
   }
   
   isLoggedIn = false;
   errorMessage = '';
 
   sumbitUsername() {
-    if (this.USERNAME != null && this.PASS != null) {
-      this.authService.login(this.USERNAME.value,this.PASS.value).subscribe(
-        data => {
-        if(data.role=="Admin"){    
-            this.tokenStorage.saveToken(data.token);
-            this.tokenStorage.saveUser(data.username);
-            this.isLoggedIn = true;
-            this.router.navigate(['home/dashboard']);
-          }else{
-            this.errorMessage = "خطأ في اسم المستخدم او كلمة سر";
-          }
-        },
-        err => {
-          this.errorMessage = "خطأ في اسم المستخدم او كلمة سر";
-        }
+  //   if (this.USERNAME != null && this.PASS != null) {
+  //     this.authService.login(this.USERNAME.value,this.PASS.value).subscribe(
+  //       data => {
+  //       if(data.role=="Admin"){    
+  //           this.tokenStorage.saveToken(data.token);
+  //           this.tokenStorage.saveUser(data.username);
+  //           this.isLoggedIn = true;
+  //           this.router.navigate(['home/dashboard']);
+  //         }else{
+  //           this.errorMessage = "خطأ في اسم المستخدم او كلمة سر";
+  //         }
+  //       },
+  //       err => {
+  //         this.errorMessage = "خطأ في اسم المستخدم او كلمة سر";
+  //       }
 
-      );
+  //     );
 
-  }
+  // }
+  this.router.navigate(['home/dashboard']);
 }
 }
 
